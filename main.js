2a72ef1b485afc45b69c5dc4a3959b192860155a
profmariapicoli <mariapicoli@prof.educacao.sp.gov.br>
2024-11-04 13:37:11 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
-let nomeUsuário = "";
-let elemento = document.querySelector("#nome-usuário");
-
-while(nomeUsuário == ""){
-    nomeUsuário = prompt("Qual o seu nome?");
-}
-
-if(nomeUsuário == null){
-    elemento.textContent = 'seja muito bem-vindo.';
-}else{
-    elemento.textContent = nomeUsuário;
-}
+document.addEventListener("DOMContentLoaded", function() {
+    const nomeUsuario = document.getElementById("nome-usuário");
+    const nome = prompt("Por favor, insira seu nome:");
+    if (nome) {
+        nomeUsuario.textContent = nome;
+    }
+});
